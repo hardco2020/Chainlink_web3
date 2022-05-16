018f8c688d79d5e6d9b5907def3dd8fb344e4f02
Kai <hardco2020@gmail.com>
2022-05-16 13:35:07 +0800

    add: install react-icons & add bottom layout

--- client/src/components/Bottom.jsx
+++ client/src/components/Bottom.jsx
@@ -1,11 +1,98 @@
 import React from "react";
+import {
+  AiOutlineArrowLeft,
+  AiOutlineWifi,
+  AiOutlineClose,
+} from "react-icons/ai";
+import {
+  BsFillCameraVideoFill,
+  BsFillCameraVideoOffFill,
+  BsFillMicMuteFill,
+  BsFillMicFill,
+  BsRecordCircle,
+  BsFullscreen,
+} from "react-icons/bs";
+import { GrMore } from "react-icons/gr";
+import { MdOutlineScreenShare } from "react-icons/md";
 
 const style = {
-  wrapper: `w-full h-full bg-gray-900 `,
+  wrapper: `flex justify-center w-full h-full bg-gray-900 text-white`,
+  container: `flex justify-center items-center`,
+  icon: `flex relative rounded-full w-10 h-10 bg-slate-100 mr-3`,
 };
 
 const Bottom = () => {
-  return <div className={style.wrapper}>Bottom</div>;
+  return (
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <div className={style.icon}>
+          <BsFillMicFill
+            style={{
+              color: "black",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              margin: "auto",
+            }}
+          />
+        </div>
+        <div className={style.icon}>
+          <BsRecordCircle
+            style={{
+              color: "black",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              margin: "auto",
+            }}
+          />
+        </div>
+        <div className={style.icon}>
+          <BsFillCameraVideoFill
+            style={{
+              color: "black",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              margin: "auto",
+            }}
+          />
+        </div>
+        <div className={style.icon}>
+          <BsFullscreen
+            style={{
+              color: "black",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              margin: "auto",
+            }}
+          />
+        </div>
+        <div className={style.icon}>
+          <MdOutlineScreenShare
+            style={{
+              color: "black",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              margin: "auto",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Bottom;
--- client/src/components/Stream.jsx
+++ client/src/components/Stream.jsx
@@ -6,10 +6,10 @@ import StreamElement from "./StreamElement";
 const style = {
   wrapper: `w-screen h-[90vh] p-4`,
   container: ` h-full border-8 p-1`,
-  topController: `flex h-[75vh]`,
+  topController: `flex h-[73vh]`,
   streamContainer: `flex w-3/5 h-full `,
   chatContainer: `flex w-2/5 h-full`,
-  bottomController: `flex w-full h-[7vh]`,
+  bottomController: `flex w-full h-[9vh]`,
 };
 
 const Stream = () => {
