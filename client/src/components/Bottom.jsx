e14640d871b232c5ea8365b15c652e0905e1e585
Kai <hardco2020@gmail.com>
2022-05-16 13:47:32 +0800

    refactor: refactor bottom button

--- client/src/components/Bottom.jsx
+++ client/src/components/Bottom.jsx
@@ -20,75 +20,34 @@ const style = {
   container: `flex justify-center items-center`,
   icon: `flex relative rounded-full w-10 h-10 bg-slate-100 mr-3`,
 };
+const IconStyle = {
+  color: "black",
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  margin: "auto",
+};
 
 const Bottom = () => {
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
         <div className={style.icon}>
-          <BsFillMicFill
-            style={{
-              color: "black",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              margin: "auto",
-            }}
-          />
+          <BsFillMicFill style={IconStyle} />
         </div>
         <div className={style.icon}>
-          <BsRecordCircle
-            style={{
-              color: "black",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              margin: "auto",
-            }}
-          />
+          <BsRecordCircle style={IconStyle} />
         </div>
         <div className={style.icon}>
-          <BsFillCameraVideoFill
-            style={{
-              color: "black",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              margin: "auto",
-            }}
-          />
+          <BsFillCameraVideoFill style={IconStyle} />
         </div>
         <div className={style.icon}>
-          <BsFullscreen
-            style={{
-              color: "black",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              margin: "auto",
-            }}
-          />
+          <BsFullscreen style={IconStyle} />
         </div>
         <div className={style.icon}>
-          <MdOutlineScreenShare
-            style={{
-              color: "black",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              margin: "auto",
-            }}
-          />
+          <MdOutlineScreenShare style={IconStyle} />
         </div>
       </div>
     </div>
